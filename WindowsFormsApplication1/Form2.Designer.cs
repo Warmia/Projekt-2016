e714a84a46b1f2583d16b80d91d929bd44d69191
Warmia <mazury21@onet.pl>
2016-11-22 01:27:29 +0100

    I znowu commit

--- WindowsFormsApplication1/Form2.Designer.cs
+++ WindowsFormsApplication1/Form2.Designer.cs
@@ -28,61 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnForecast = new System.Windows.Forms.Button();
+            this.lvwTemps = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(142, 48);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Temperature:";
             // 
-            // textBox2
+            // label2
             // 
-            this.textBox2.Location = new System.Drawing.Point(142, 95);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 0;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Location:";
             // 
-            // textBox3
+            // txtLocation
             // 
-            this.textBox3.Location = new System.Drawing.Point(142, 135);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 0;
+            this.txtLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLocation.Location = new System.Drawing.Point(69, 10);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(219, 20);
+            this.txtLocation.TabIndex = 20;
+            this.txtLocation.Text = "Denver, CO";
             // 
-            // textBox4
+            // btnForecast
             // 
-            this.textBox4.Location = new System.Drawing.Point(142, 182);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 0;
+            this.btnForecast.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnForecast.Location = new System.Drawing.Point(113, 36);
+            this.btnForecast.Name = "btnForecast";
+            this.btnForecast.Size = new System.Drawing.Size(75, 23);
+            this.btnForecast.TabIndex = 21;
+            this.btnForecast.Text = "Forecast";
+            this.btnForecast.UseVisualStyleBackColor = true;
+            this.btnForecast.Click += new System.EventHandler(this.btnForecast_Click);
             // 
-            // textBox5
+            // lvwTemps
             // 
-            this.textBox5.Location = new System.Drawing.Point(142, 220);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 0;
+            this.lvwTemps.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwTemps.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvwTemps.Location = new System.Drawing.Point(15, 79);
+            this.lvwTemps.Name = "lvwTemps";
+            this.lvwTemps.Size = new System.Drawing.Size(273, 116);
+            this.lvwTemps.TabIndex = 24;
+            this.lvwTemps.UseCompatibleStateImageBehavior = false;
+            this.lvwTemps.View = System.Windows.Forms.View.Details;
             // 
-            // Form2
+            // columnHeader1
             // 
+            this.columnHeader1.Text = "Day";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Time";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Temperature";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 86;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnForecast;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 312);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form2";
-            this.Text = "Form2";
-            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ClientSize = new System.Drawing.Size(300, 207);
+            this.Controls.Add(this.lvwTemps);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnForecast);
+            this.Name = "Form1";
+            this.Text = "howto_list_temperatures";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,11 +129,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnForecast;
+        private System.Windows.Forms.ListView lvwTemps;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
--- WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/Form2.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Globalization;
 
 namespace WindowsFormsApplication1
 {
@@ -21,5 +25,72 @@ namespace WindowsFormsApplication1
         {
             
         }
+        private const string API_KEY = "f8dc63a23acc6e0e69070a66a3c01c0a";
+        private const string ForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?" + "q=@LOC@&mode=xml&units=Metric&APPID=" + API_KEY;
+
+
+
+        private void btnForecast_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+
+            string url = ForecastUrl.Replace("@LOC@", txtLocation.Text);
+            XmlDocument xml_doc;
+            using (WebClient client = new WebClient())
+            {
+                
+                string xml = client.DownloadString(url);
+
+                
+                xml_doc = new XmlDocument();
+                xml_doc.LoadXml(xml);
+            }
+            ListTemperatures(xml_doc);
+
+            Cursor = Cursors.Default; 
+        }
+        private void ListTemperatures(XmlDocument xml_doc)
+        {
+            lvwTemps.Items.Clear();
+
+            
+            string last_day = "";
+            foreach (XmlNode time_node in xml_doc.SelectNodes("//time"))
+            {
+                
+                XmlAttribute time_attr = time_node.Attributes["from"];
+                DateTime start_time = DateTime.Parse(time_attr.Value);
+
+                
+                start_time = start_time.ToLocalTime();
+
+                
+                start_time += new TimeSpan(1, 30, 0);
+
+                
+                XmlNode temp_node = time_node.SelectSingleNode("temperature");
+                XmlAttribute temp_attr = temp_node.Attributes["value"];
+                float temp = 0;
+                if (temp_attr != null)
+                {
+                    temp = float.Parse(temp_attr.Value.ToString(), CultureInfo.InvariantCulture);
+                }
+
+                ListViewItem item;
+                if (start_time.DayOfWeek.ToString() == last_day)
+                {
+                    item = lvwTemps.Items.Add("");
+                }
+                else
+                {
+                    last_day = start_time.DayOfWeek.ToString();
+                    item = lvwTemps.Items.Add(last_day);
+                }
+                item.SubItems.Add(start_time.ToShortTimeString());
+                item.SubItems.Add(temp.ToString("0.00"));
+            }
+        }
+
+
     }
 }
